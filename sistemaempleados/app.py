fd01ed88b0853561cbbf2e0849b33908351dbbf5
Ignacio Viggiani <ign.v@hotmail.com>
2022-08-25 17:30:20 -0300

    db, employee creation working

--- sistemaempleados/app.py
+++ sistemaempleados/app.py
@@ -1,26 +1,42 @@
 from flask import Flask
-from flask import render_template
-# from flaskext.mysql import MySQL
+from flask import render_template, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-# mysql = MySQL()
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = ''
-# app.config['MYSQL_DATABASE_BD'] = 'sistema'
-# mysql.init_app(app)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_BD'] = 'sistema'
+mysql.init_app(app)
 
 @app.route('/')
-
 def index():
+
 	return render_template('empleados/index.html')
-	
-# sql = ""
-# conn = mysql.connect()
-# cursor = conn.cursor()
-# cursor.execute(sql)
-# conn.commit()
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+	return render_template('empleados/create.html')
+
+@app.route('/store', methods=['GET', 'POST'])
+def storage():
+
+	_nombre = request.form['txtNombre']
+	_correo = request.form['txtCorreo']
+	_foto = request.files['txtFoto']
+
+	sql = "INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+
+	datos = (_nombre,_correo,_foto.filename)
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute(sql, datos)
+	conn.commit()
+
+	return render_template('empleados/index.html')
 
 if __name__ == '__main__':
 	app.run(debug = True)
