e61b8980246085a96b28017591db6618abfba8b0
Ignacio Viggiani <ign.v@hotmail.com>
2022-08-25 17:45:17 -0300

    pic archive ok, no name/copy problem on pics

--- sistemaempleados/app.py
+++ sistemaempleados/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -28,9 +29,15 @@ def storage():
 	_correo = request.form['txtCorreo']
 	_foto = request.files['txtFoto']
 
+	now = datetime.now()
+	tiempo = now.strftime('%Y%H%M%S')
+	if _foto.filename!='':
+		nuevoNombreFoto = tiempo + _foto.filename
+		_foto.save('uploads/' + nuevoNombreFoto)
+
 	sql = "INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
 
-	datos = (_nombre,_correo,_foto.filename)
+	datos = (_nombre,_correo,nuevoNombreFoto)
 	conn = mysql.connect()
 	cursor = conn.cursor()
 	cursor.execute(sql, datos)
