dda9cdca701e2bab8be99e04907031438a28d5c8
Ignacio Viggiani <ign.v@hotmail.com>
2022-08-29 11:06:52 -0300

    edit function working, no pic update

--- sistemaempleados/app.py
+++ sistemaempleados/app.py
@@ -5,12 +5,16 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# Database Connection (localhost, XAMPP outside environment)
+
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_BD'] = 'sistema'
 mysql.init_app(app)
+
+# Index page, 'main menu'. Shows current state of employees database.
 
 @app.route('/')
 def index():
@@ -22,6 +26,8 @@ def index():
 	conn.commit()
 	return render_template('empleados/index.html', empleados=empleados)
 
+# Delete function called within index with SQL query, refresh index.
+
 @app.route('/destroy/<int:id>')
 def destroy(id):
 	conn = mysql.connect()
@@ -30,10 +36,14 @@ def destroy(id):
 	conn.commit()
 	return redirect('/')
 
+# Create employee method
+
 @app.route('/create', methods=['GET', 'POST'])
 def create():
 
 	return render_template('empleados/create.html')
+
+# After succsessfully creating an employee (form completion), insert into database.
 
 @app.route('/store', methods=['GET', 'POST'])
 def storage():
@@ -56,7 +66,42 @@ def storage():
 	cursor.execute(sql, datos)
 	conn.commit()
 
-	return render_template('empleados/index.html')
+	return redirect('/')
+
+# Function that permits editing an existing employee
+
+@app.route('/edit/<int:id>')
+def edit(id):
+
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM `sistema`.`empleados` WHERE id=%s", (id))
+	empleados=cursor.fetchall()
+	conn.commit()
+
+	return render_template('empleados/edit.html', empleados=empleados)
+
+# Update function after edit template runs and confirms a commit to the database
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+
+	_nombre=request.form['txtNombre']
+	_correo=request.form['txtCorreo']
+	_foto=request.files['txtFoto']
+	id=request.form['txtID']
+
+	sql = "UPDATE `sistema`.`empleados` SET `nombre`=%s, `correo`=%s WHERE id=%s;"
+	datos=(_nombre,_correo,id)
+
+	conn = mysql.connect()
+	cursor = conn.cursor()
+
+	cursor.execute(sql,datos)
+	conn.commit()
+	
+	return redirect('/')
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
